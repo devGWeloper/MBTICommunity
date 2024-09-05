f02b24707da7f9a7298f9774042fc97cbb91e171
devGWeloper <devgweloper@gmail.com>
2024-09-05 23:09:02 +0900

    feature test

--- BackEnd/mbti/src/main/java/com/kumoh/mbti/controller/PostController.java
+++ BackEnd/mbti/src/main/java/com/kumoh/mbti/controller/PostController.java
@@ -3,8 +3,10 @@ package com.kumoh.mbti.controller;
 import com.kumoh.mbti.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+// ResposeBody 내포, View대신 html body에 원문을 찍어보냄
+@RestController
 public class PostController {
 
     @Autowired
